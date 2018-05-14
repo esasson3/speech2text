33fc5e5aaee092b8e8d0da5dedb84b0e1439c9d9
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-05-14 16:11:26 -0700

    adds rite

--- public/js/analyze.js
+++ public/js/analyze.js
@@ -70,15 +70,10 @@ if (!('webkitSpeechRecognition' in window)) {
                 interim_transcript += event.results[i][0].transcript;
             }
         }
-        
-        
+
+
         var shown_transcript = final_transcript;
-//        getToneAnalysis(shown_transcript);
-        
-        // analyze shown_transcript
-        // use analyzer results to change final_transcript
-        
-        
+
         final_transcript = capitalize(final_transcript);
         final_span.innerHTML = linebreak(final_transcript);
         interim_span.innerHTML = linebreak(interim_transcript);
@@ -106,38 +101,6 @@ function capitalize(s) {
         return m.toUpperCase();
     });
 }
-
-//function createEmail() {
-//    var n = final_transcript.indexOf('\n');
-//    if (n < 0 || n >= 80) {
-//        n = 40 + final_transcript.substring(40).indexOf(' ');
-//    }
-//    var subject = encodeURI(final_transcript.substring(0, n));
-//    var body = encodeURI(final_transcript.substring(n + 1));
-//    window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
-//}
-
-//function copyButton() {
-//    if (recognizing) {
-//        recognizing = false;
-//        recognition.stop();
-//    }
-//    copy_info.style.display = 'inline-block';
-//    showInfo('');
-//}
-
-//function emailButton() {
-//    if (recognizing) {
-//        create_email = true;
-//        recognizing = false;
-//        recognition.stop();
-//    } else {
-//        createEmail();
-//    }
-//    email_button.style.display = 'none';
-//    email_info.style.display = 'inline-block';
-//    showInfo('');
-//}
 
 function startButton(event) {
     if (recognizing) {
@@ -238,7 +201,7 @@ function allReady(thresholds, sampleText) {
     $error.hide();
     $output.show();
     // scrollTo($output);
-      
+
     console.log(data)
 
     var emotionTone = data.document_tone.tones.slice(0),
@@ -271,7 +234,7 @@ function allReady(thresholds, sampleText) {
     sentenceTone = Object.keys(sentenceTone).sort().map(function(obj) {
       return sentenceTone[obj];
     });
-      
+
     console.log(sentenceTone);
     var i;
     for (i = 0; i < sentenceTone.length; i++) {
@@ -286,7 +249,7 @@ function allReady(thresholds, sampleText) {
             final_transcipt.italics();
         }
     }
-       
+
     final_transcript = capitalize(final_transcript);
     final_span.innerHTML = linebreak(final_transcript);
     interim_span.innerHTML = linebreak(interim_transcript);
@@ -315,7 +278,7 @@ function allReady(thresholds, sampleText) {
     }
 
     emotionTone = app.getDocumentToneDefault();
-      
+
     // Update scores for the tones present in response at document level
     if (typeof(data.document_tone.tones) !== 'undefined' && data.document_tone.tones !== null) {
       data.document_tone.tones.forEach(function(element) {
